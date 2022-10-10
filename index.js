95bf95e111661a1e083a03b1992a24f49733251a
Dmytro <herasymdmytro21@gmail.com>
2022-10-10 23:45:46 +0300

    alpha version 1.2

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const myDiv = document.createElement('div')
 const input = document.createElement('input')
 const buttonAdd = document.createElement('button')
 const myList = document.createElement('ul')
-const items = document.createElement('li')
+const item = document.createElement('li')
 const buttonClear = document.createElement('button')
 const buttonClose = document.createElement('button')
+const buttonEdit = document.createElement('button')
 
 buttonClose.classList.add('close')
-
 
 buttonClear.textContent = 'clear'
 
@@ -20,8 +20,9 @@ myDiv.classList.add('list')
 input.classList.add('input')
 buttonAdd.classList.add('add')
 myList.classList.add('my-list')
-items.classList.add('item')
+item.classList.add('item')
 buttonClear.classList.add('clear')
+buttonEdit.classList.add('edit')
 
 buttonAdd.textContent = 'add'
 wrapper.append(myDiv)
@@ -30,33 +31,52 @@ myDiv.append(buttonAdd)
 wrapper.append(myList)
 wrapper.append(buttonClear)
 
-buttonAdd.addEventListener('click', () => {
+
+const addItemToList = () => {
 	if (input.value === '') {
-		alert('write text...')
-	} else {
-		const items = document.createElement('li')
-		items.classList.add('list-item')
-		myList.append(items)
+	alert('write text...')
+} else {
+		const item = document.createElement('li')
+		const itemContent = document.createElement('span')
 		const buttonClose = document.createElement('button')
+		const buttonEdit = document.createElement('button')
+
 		buttonClose.classList.add('close')
+		item.classList.add('list-item')
+		buttonEdit.classList.add('edit')
 		buttonClose.textContent = 'x'
-		myList.append(buttonClose)
-		items.textContent = input.value
-		input.value = ''
-		buttonClose.addEventListener('click', (e) => {
-			items.textContent = ''
-		})
+		buttonEdit.textContent = 'edit'
+
+		myList.append(item)
+		item.append(itemContent);
+		item.append(buttonClose)
+		item.append(buttonEdit)
+
+		itemContent.textContent = input.value
+		input.value = ''	
+		
 		buttonClose.addEventListener('click', () => {
-			buttonClose.textContent = ''
-			// myList.textContent = ''
-			items.style.cssText = 'display: none'
-			
+			item.remove();
 		})
-	}
 
-})
+		const editList = () => {
+			buttonEdit.textContent = 'test'
+			itemContent.textContent = input.value
 
-buttonClear.addEventListener('click', () => {
+		};
+
+		buttonEdit.addEventListener('click', editList);
+	}	
+}
+
+const clearList = () => {
+	buttonClear.addEventListener('click', () => {
 	myList.textContent = ''
 })
+
+}
+
+buttonAdd.addEventListener('click', addItemToList)
+buttonClear.addEventListener('click', clearList)
+
 
